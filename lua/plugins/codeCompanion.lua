c0337613c7812226db43b815fb923e676007229c
Horon_Lee <59383381+HoronLee@users.noreply.github.com>
2025-03-26 20:22:48 +0800

    增加CodeConpanion的硅基流动配置文件

--- lua/plugins/codeCompanion.lua
+++ lua/plugins/codeCompanion.lua
@@ -8,9 +8,9 @@ return {
     },
     opts = {
       strategies = {
-        chat = { adapter = "deepseek" },
-        inline = { adapter = "deepseek" },
-        agent = { adapter = "deepseek" },
+        chat = { adapter = "siliconflow_v3" },
+        inline = { adapter = "siliconflow_v3" },
+        agent = { adapter = "siliconflow_v3" },
       },
       adapters = {
         deepseek = function()
@@ -24,6 +24,48 @@ return {
             schema = {
               model = {
                 default = "deepseek-chat",
+              },
+            },
+          })
+        end,
+
+        siliconflow_r1 = function()
+          return require("codecompanion.adapters").extend("deepseek", {
+            name = "siliconflow_r1",
+            url = "https://api.siliconflow.cn/v1/chat/completions",
+            env = {
+              api_key = function()
+                return os.getenv("DEEPSEEK_API_KEY_S")
+              end,
+            },
+            schema = {
+              model = {
+                default = "deepseek-ai/DeepSeek-R1",
+                choices = {
+                  ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+                  "deepseek-ai/DeepSeek-V3",
+                },
+              },
+            },
+          })
+        end,
+
+        siliconflow_v3 = function()
+          return require("codecompanion.adapters").extend("deepseek", {
+            name = "siliconflow_v3",
+            url = "https://api.siliconflow.cn/v1/chat/completions",
+            env = {
+              api_key = function()
+                return os.getenv("DEEPSEEK_API_KEY_S")
+              end,
+            },
+            schema = {
+              model = {
+                default = "deepseek-ai/DeepSeek-V3",
+                choices = {
+                  "deepseek-ai/DeepSeek-V3",
+                  ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+                },
               },
             },
           })
