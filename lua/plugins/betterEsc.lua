e2334a9846ffb2ec33e9a2b1c1da26aec7e5bb3b
Horon_Lee <59383381+HoronLee@users.noreply.github.com>
2025-04-10 14:21:12 +0800

    添加better-escape插件

---
+++ lua/plugins/betterEsc.lua
@@ -0,0 +1,10 @@
+return {
+  "max397574/better-escape.nvim",
+  event = "InsertEnter",
+  opts = {
+    mapping = { "jj" }, -- 你想用来触发 Esc 的键，比如 "jj"
+    timeout = 300, -- 毫秒，超时就不会触发 Esc，默认是 300
+    clear_empty_lines = false,
+    keys = "<Esc>",
+  },
+}
