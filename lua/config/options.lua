6fb5ee739636b38aff7d47cf795f1da9b2a313b0
HoronLee <horonlee@foxmail.com>
2025-05-09 11:24:51 +0800

    更新类 unix 系统优先使用 fish shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,10 +4,7 @@
 -- 通过系统类型来选择nvim的终端，在linux优先选择zsh，如果没有则使用bash
 if vim.fn.has("win32") == 1 then
   LazyVim.terminal.setup("pwsh")
-elseif vim.fn.has("linux") then
-  if vim.fn.executable("zsh") == 1 then
-    LazyVim.terminal.setup("zsh")
-  else
-    LazyVim.terminal.setup("bash")
-  end
+elseif vim.fn.has("unix") == 1 then
+  local shell = vim.fn.executable("fish") == 1 and "fish" or vim.fn.executable("zsh") == 1 and "zsh" or "bash"
+  LazyVim.terminal.setup(shell)
 end
