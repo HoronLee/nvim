d84b2d20b66a75556ecdcb4a3871b690583abc52
HoronLee <horonlee@foxmail.com>
2025-06-09 00:51:10 +0800

    更新终端选择器

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- 通过系统类型来选择nvim的终端，在linux优先选择zsh，如果没有则使用bash
+
+-- 通过系统类型来选择 nvim 的终端，在linux优先选择 fish ，如果没有则使用 zsh 或者 bash
 if vim.fn.has("win32") == 1 then
   LazyVim.terminal.setup("pwsh")
 elseif vim.fn.has("unix") == 1 then
