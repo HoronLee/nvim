41dad33cb36c9e7fe8f6d643ea7e2b3b775f696b
HoronLee <horonlee@foxmail.com>
2025-06-24 16:44:05 +0800

    修改主题设置

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -57,21 +57,21 @@ return {
       },
     },
   },
-  {
-    "sainnhe/everforest",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      -- 配置 Everforest 主题
-      vim.g.everforest_enable_italic = true
-      vim.cmd.colorscheme("everforest") -- 设置 Everforest 作为默认主题
-    end,
-  },
-  {
-    "morhetz/gruvbox",
-    lazy = true, -- 延迟加载 Grubox，可以根据需要手动加载
-    config = function()
-      vim.cmd.colorscheme("gruvbox") -- 设置 Gruvbox 作为主题
-    end,
-  },
+  -- {
+  --   "sainnhe/everforest",
+  --   lazy = true,
+  --   priority = 1000,
+  --   config = function()
+  --     -- 配置 Everforest 主题
+  --     vim.g.everforest_enable_italic = true
+  --     vim.cmd.colorscheme("everforest") -- 设置 Everforest 作为默认主题
+  --   end,
+  -- },
+  -- {
+  --   "morhetz/gruvbox",
+  --   lazy = true, -- 延迟加载 Grubox，可以根据需要手动加载
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox") -- 设置 Gruvbox 作为主题
+  --   end,
+  -- },
 }
